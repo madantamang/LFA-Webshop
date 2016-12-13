e9b43339e998a9ac97b3c4af3767b92008c60f96
Aman <shakya_aman17@hotmail.com>
2016-12-13 08:11:18 +0545

    added orderedProductModel

---
+++ Web-codefirst/Models/OrderedProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_codefirst.Models
+{
+    public class OrderedProductModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public int CustomerOrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
